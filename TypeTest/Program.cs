6dbff5c43ccc9ab75a33ae0a9ec0fbc1f5c59107
Ers28203006 <ersyn_sultanbaev@mail.ru>
2019-03-17 02:25:56 +0600

    Добавьте файлы проекта.

---
+++ TypeTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace TypeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" 1.С помощью рефлексии вызвать метод Substring класса String и извлечь из строки подстроку заданного размера.");
+
+            string str = "Древние греки придумали Олимпийские игры, пока вели одну из своих " +
+              "нескончаемых войн.";
+
+            Type type = typeof(String);
+
+            MethodInfo substring = type.GetMethod("Substring",
+                new Type[] { typeof(int), typeof(int) });
+
+            Object result = substring.Invoke(str, new Object[] { 0, 40 });
+            Console.WriteLine("{0}\n\"{1}\".\n\n", substring, result);
+
+            Console.WriteLine(" 2.Получить список конструкторов класса List<T>");
+            type = typeof(List<>);
+            foreach (ConstructorInfo ctor in type.GetConstructors())
+            {
+                Console.WriteLine(ctor);
+            }
+
+            Console.WriteLine("\n\n1.С помощью рефлексии получить список методов класса Console и вывести на экран.");
+            type = typeof(Console);
+            foreach (MethodInfo method in type.GetMethods())
+            {
+                Console.WriteLine(method);
+            }
+
+            Console.WriteLine("\n\n2.Описать класс с несколькими свойствами. Создать экземпляр класса и инициализировать его свойства.\n"+ 
+            "С помощью рефлексии получить свойства и их значения из созданного экземпляра класса. \nВывести полученные значения на экран");
+
+            Student student = new Student("Алеша", "Попович", 25);
+            type = typeof(Student);
+            Console.WriteLine("Свойства класса: ");
+            foreach (PropertyInfo property in type.GetProperties())
+            {
+                Console.Write(property.Name);
+                Console.WriteLine(": "+property.GetValue(student));
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ TypeTest/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeTest
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Student()
+        {
+
+        }
+
+        public Student(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public void ShowStudentInfo()
+        {
+            Console.WriteLine("Имя: {0}", Name);
+            Console.WriteLine("Фамилия: {0}", Surname);
+            Console.WriteLine("Возраст: {0}", Age);
+        }
+    }
+}
